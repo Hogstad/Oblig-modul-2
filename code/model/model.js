5859c42f4ccd2344f7806bcd5d902334be6122e8
Flemming Hogstad <flemming.hogstad@gmail.com>
2021-01-25 12:45:25 +0100

    ???

---
+++ code/model/model.js
@@ -0,0 +1,24 @@
+const divApp = document.getElementById("app");
+
+const model = {
+        boxes: [
+            {
+                name: 'Box 1',
+                content: [
+                    {contentOfBox: 'greier1', isChecket: false },
+                    { contentOfBox: 'greier2', isChecket: false },
+                    {contentOfBox: 'greier3', isChecket: false },
+                    {contentOfBox: 'greier4', isChecket: false },
+                ],
+            },
+            {
+                name: 'Box 2',
+                content: [
+                    {contentOfBox: 'greier5', isChecket: false },
+                    {contentOfBox: 'greier6', isChecket: false },
+                    {contentOfBox: 'greier7', isChecket: false },
+                    {contentOfBox: 'greier8', isChecket: false },
+            },
+            
+        ],
+},
