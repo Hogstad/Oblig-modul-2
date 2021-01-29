b83f4d3a595589c5797ad808c65dd346065425ec
Flemming Hogstad <flemming.hogstad@gmail.com>
2021-01-29 13:18:52 +0100

    Update controller.js

--- code/controller/controller.js
+++ code/controller/controller.js
@@ -14,9 +14,15 @@
 
     //Endre Valgt
     //Sende deg inn på subView, innenfor den boksen som er valgt.
+    function changeBox {
+        
+    }
 
     //Søk etter innhold
     //Søkeboks som man kan søke igjennom gjenstander i alle boksene i modellen.
+    function search {
+
+    }
 
     //Slett Valgt
     //Slette den markerte boksen.
@@ -28,12 +34,25 @@
 //subView
     //Legg til ny
     //Legge til nytt innhold innenfor den valgte boksen.
+    function addNewContentInBox {
+
+    }
 
     //Endre valgt
     //Her skal man kunne endre den valgte gjenstanden som er inne i en ny boks (navn)
+    function changeContentInBox {
+
+    }
 
     //flytt
     //Flytte en gjenstand fra boks x til box y f.eks.
+    function moveContentfromBox {
+
+    }
 
     //Slett
     //Slette den valgte gjenstanden innenfor den valgte boksen.
+    function deleteContentInBox(index) {
+        model.boxes[0].content[0].splice(index, 1);
+        mainView();
+    }
